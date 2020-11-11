ee3fb1e33124da81780b37ac642013512e18196d
awsdert <gb2985@gmail.com>
2020-11-11 22:49:33 +0000

    Forgot to add a few operators

--- include/unic/arch.h
+++ include/unic/arch.h
@@ -20,42 +20,42 @@
 
 #if defined( __WIN64__ ) || defined( __WIN64 ) \
 	|| defined( _WIN64_ ) || defined( _WIN64 ) || defined( WIN64 ) \
-	defined( __win64__ ) || defined( __win64 ) \
+	|| defined( __win64__ ) || defined( __win64 ) \
 	|| defined( _win64_ ) || defined( _win64 ) || defined( win64 )
 #define UNIC__SYS_WIN64
 #endif
 
 #if defined( __WIN32__ ) || defined( __WIN32 ) \
 	|| defined( _WIN32_ ) || defined( _WIN32 ) || defined( WIN32 ) \
-	defined( __win32__ ) || defined( __win32 ) \
+	|| defined( __win32__ ) || defined( __win32 ) \
 	|| defined( _win32_ ) || defined( _win32 ) || defined( win32 )
 #define UNIC__SYS_WIN32
 #endif
 
 #if defined( __WIN16__ ) || defined( __WIN16 ) \
 	|| defined( _WIN16_ ) || defined( _WIN16 ) || defined( WIN16 ) \
-	defined( __win16__ ) || defined( __win16 ) \
+	|| defined( __win16__ ) || defined( __win16 ) \
 	|| defined( _win16_ ) || defined( _win16 ) || defined( win16 )
 #define UNIC__SYS_WIN16
 #endif
 
 #if defined( __UNIX__ ) || defined( __UNIX ) \
 	|| defined( _UNIX_ ) || defined( _UNIX ) || defined( UNIX ) \
-	defined( __unix__ ) || defined( __unix ) \
+	|| defined( __unix__ ) || defined( __unix ) \
 	|| defined( _unix_ ) || defined( _unix ) || defined( unix )
 #define UNIC__SYS_UNIX
 #endif
 
 #if defined( __LINUX__ ) || defined( __LINUX ) \
 	|| defined( _LINUX_ ) || defined( _LINUX ) || defined( LINUX ) \
-	defined( __linux__ ) || defined( __linux ) \
+	|| defined( __linux__ ) || defined( __linux ) \
 	|| defined( _linux_ ) || defined( _linux ) || defined( linux )
 #define UNIC__SYS_LINUX
 #endif
 
 #if defined( __POSIX__ ) || defined( __POSIX ) \
 	|| defined( _POSIX_ ) || defined( _POSIX ) || defined( POSIX ) \
-	defined( __posix__ ) || defined( __posix ) \
+	|| defined( __posix__ ) || defined( __posix ) \
 	|| defined( _posix_ ) || defined( _posix ) || defined( posix )
 #define UNIC__SYS_POSIX
 #endif
